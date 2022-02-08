6ee3d81f3d4efbe814196860e13895e1bc26d39c
Camilla3004 <94189154+Cami3004@users.noreply.github.com>
2022-02-08 11:09:25 +0000

    es corretto

--- lezione4.py
+++ lezione4.py
@@ -2,12 +2,13 @@ class CSVFile():
     def __init__(self, name):
         self.name=name
     def get_data(self):
-       my_file = open(self.name, 'r')
-       righe=[] #lista vuota
+       righe=[] #inizializzo una lista vuota
        for line in my_file:
            righe.append(line)
-       return righe 
-    
-csv_file=CSVFile('sales.txt') #csv_file istanza
-print(csv_file.get_data())
+       return righe
+       my_file.close()
 
+my_file=open('sales.txt', 'r')
+csv_file=CSVFile('sales.txt') #csv_file è un'istanza
+print('Nome del file: {}'. format(csv_file.name))
+print('Dati contenuti nel file: {}'.format(csv_file.get_data()))
