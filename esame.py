7490b5015d466655800b1fc67f6e513eeab953b6
Camilla3004 <94189154+Cami3004@users.noreply.github.com>
2022-02-21 14:48:22 +0000

    funzione finita. nomi da cambiare e da finire prima parte

--- esame.py
+++ esame.py
@@ -59,8 +59,8 @@ class CSVTimeSeriesFile():
                             print('Errore, questo valore dei passeggeri non è intero positivo: {}'.format(passeggeri))
                             continue
 
-            time_series.append([f"{anno}-{str(mese).zfill(2)}",passeggeri]) #in questo modo inserisco la data sotto forma di stringa mentre il numero dei passeggeri come numero
-            #spiegare o sostituire zfill
+            time_series.append([f"{anno}-{mese}",passeggeri]) #in questo modo inserisco la data sotto forma di stringa mentre il numero dei passeggeri come numero
+            #spiegare o sostituire zfill, ma effettivamente serve?????
            
         my_file.close()
         #ora ho tutti i dati "puliti", cioè ho tutti i dati che cercato (data,passeggeri)
@@ -73,11 +73,11 @@ class CSVTimeSeriesFile():
         for item in lista_date:
             if(lista_date.count(item)>1):
                 raise ExamException('Errore, la seguente data è ripetuta: {}'.format(item))
-    
+        #ora controllo se è ordinata
+        #anno e mese in questo mese sono stringhe sono int ed esistono????
         return time_series
 
 def detect_similar_monthly_variations(time_series, years):
-    #oppure posso mettere fist_year=years[0] e last_years=years[1]
     #controllo la validità della lista years:
     #controllo che years sia una lista
     if not isinstance(years, list):
@@ -85,17 +85,83 @@ def detect_similar_monthly_variations(time_series, years):
     #controllo che la lista abbia precisamente due valori
     if (len(years)!=2):
         raise ExamException('Errore, la lista inserita non può essere considerata perchè non ha due due valori ma ne ha: {}'.format(len(years)))
-    else: #ha senso mettere if dentro else o no??
-        #controllo che i due valori siano degli interi
-        if not (isinstance(years[0], int) or isinstance(years[1], int)):
-            raise ExamException('Errore, gli elementi della lista devono essere due numeri interi')
+    #controllo che i due valori siano degli interi
+    if (isinstance(years[0], int)==False or isinstance(years[1], int)==False):
+        raise ExamException('Errore, gli elementi della lista devono essere due numeri interi')
 
-        if (years[0]<1949 or years[1]>1960):
-            raise ExamException("Errore, l'intervallo non è valido, perchè si stanno considerando degli anni non appartenenti al file")
-        elif(years[0]>=years[1]):
-            raise ExamException('Errore, range di anni non valido')
+    if (years[0]<1949 or years[1]>1960):
+        raise ExamException("Errore, l'intervallo non è valido, perchè si stanno considerando degli anni non appartenenti al file")
+    if(years[0]>=years[1]):
+        raise ExamException('Errore, range di anni non valido')
+    if(years[1]-years[0]!=1):
+        raise ExamException('Errore, range di anni non valido perchè i due anni inseriti devono essere consecutivi')
 
-        
-#my_file=open('data.csv', 'r')
+#se sono arrivata qui significa che il range di anni inseriti è valido
+#ora creo una lista di liste di tre elementi con anno,mese,passeggeri, partendo da time_series
+    lista_dati=[]
+    for item in time_series:
+        riga_dati=[]
+        for i,element in enumerate(item):
+            if i==0:
+                anno_mese=element.split('-')
+                riga_dati.append(int(anno_mese[0]))
+                riga_dati.append(int(anno_mese[1]))
+            else:
+                riga_dati.append(element)
+                continue
+    
+            lista_dati.append(riga_dati)
+
+    #utilizzo list comprehension per creare due liste con il mese e il numero dei passeggeri per quel mese considerando solo i due anni years[0] e years[1] inseriti in input
+    passeggeri_anno_0=[[item[1],item[2]] for item in lista_dati if item[0]==years[0]]
+    passeggeri_anno_1=[[item[1], item[2]] for item in lista_dati if item[0]==years[1]]
+
+    #creo due liste di 12 elementi in cui ogni elemento sarà la stringa 'nullo'
+    anno0_passeggeri=[]
+    anno1_passeggeri=[]
+    for v in range(12):
+        anno0_passeggeri.append('nullo')
+    for v in range(12):
+        anno1_passeggeri.append('nullo')
+
+    #ora sostituisco il valore 'nullo' con il numero dei passeggeri di quel mese (mese 1=gennaio, posizione 0) e nel caso in cui manca un valore rimane la stringa 'nullo'
+    #faccio due volte la stessa cose perchè ho due liste, una per years[0] e una per years[1]
+    for item in passeggeri_anno_0:
+        mese=int(item[0])
+        anno0_passeggeri[mese-1]=item[1]
+
+    for item in passeggeri_anno_1:
+        mese=item[0]
+        anno1_passeggeri[mese-1]=item[1]
+
+    differenza_anno0=[]
+    for i in range(11):
+        if(anno0_passeggeri[i]=='nullo' or anno0_passeggeri[i+1]=='nullo'):
+            differenza_anno0.append('nullo')
+        else:
+            differenza_anno0.append(anno0_passeggeri[i+1]-anno0_passeggeri[i])
+    print(differenza_anno0)
+
+    differenza_anno1=[]
+    for i in range(11):
+        if(anno1_passeggeri[i]=='nullo' or anno1_passeggeri[i+1]=='nullo'):
+            differenza_anno1.append('nullo')
+        else:
+            differenza_anno1.append(anno1_passeggeri[i+1]-anno1_passeggeri[i])
+    print(differenza_anno1)
+
+    risultato=[]
+    for i in range (11):
+        if(differenza_anno0[i]=='nullo' or differenza_anno1[i]=='nullo'):
+            risultato.append(False)
+        elif(differenza_anno0[i]-differenza_anno1[i] in range(-2,3)): #metto range(-2,3) perchè l'estremo superiore non è compreso
+            risultato.append(True)
+        else:
+            risultato.append(False)
+    print(risultato)
+
+
 csv_file=CSVTimeSeriesFile('data.csv') 
-print('Dati contenuti nel file: {}'.format(csv_file.get_data()))
+time_series=csv_file.get_data()
+lista=[1950,1951]
+result=detect_similar_monthly_variations(time_series,lista)
